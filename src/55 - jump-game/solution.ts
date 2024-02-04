429026396dde513314b16014c9390310297e7300
Izzy Beraja <IzzyBeraja+github@gmail.com>
2024-02-04 17:48:58 -0500

    55 - Jump Game (#6)
    * 55 - Jump Game
    
    * Fixes to solution, added tests and updated README

---
+++ src/55 - jump-game/solution.ts
@@ -0,0 +1,22 @@
+export function canJump(nums: number[]): boolean {
+  let jumpAmount = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    jumpAmount--;
+    if (jumpAmount < 0) return false;
+    if (nums[i] > jumpAmount) jumpAmount = nums[i];
+  }
+
+  return true;
+};
+
+/**
+ * Example 1:
+ *     [5,1,2,0,0,2]
+ * 0 - [5,1,2,0,0,2] 0->5 √
+ * 
+ * Example: 2:
+ *     [4,3,1,0,0,2]
+ * 0 - [4,3,1,0,0,2] j=4 0->4,3,2->3,2,1->4,3,2,1,0
+ * 
+ */
