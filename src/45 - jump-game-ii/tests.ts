4eb53a50de56a1dc4fdf41730950a9f36936a73f
Izzy Beraja <IzzyBeraja+github@gmail.com>
2024-02-05 12:08:53 -0500

    45 - Jump Game II

---
+++ src/45 - jump-game-ii/tests.ts
@@ -0,0 +1,23 @@
+import { jump } from './solution';
+
+describe('45 - Jump Game II', () => {
+  it('Case 1', () => {
+    expect(jump([2, 3, 1, 1, 4])).toBe(2);
+  });
+
+  it('Case 2', () => {
+    expect(jump([2, 3, 0, 1, 4])).toBe(2);
+  });
+
+  it('Case 3', () => {
+    expect(jump([1, 1, 1, 3])).toBe(3);
+  });
+
+  it('Case 4', () => {
+    expect(jump([1])).toBe(0);
+  });
+
+  it('Case 5', () => {
+    expect(jump([3, 3, 0, 0, 1, 1, 3])).toBe(4);
+  });
+});
